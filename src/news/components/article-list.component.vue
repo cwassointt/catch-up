bd8ce20f13306912fdfe5b54ccc16db04c6b84bb
cwassointt <dillpixel1@gmail.com>
2024-09-06 17:56:38 -0500

    fix(news): fixed article list component.

---
+++ src/news/components/article-list.component.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "article-list",
+  props: {
+    articles: { type: Array, required: true },
+  }
+}
+</script>
+
+<template>
+  <div v-for="article in articles" :key="articles.title">
+    <article-item :article="article"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
